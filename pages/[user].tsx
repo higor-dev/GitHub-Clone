319e7a01acc6cbd72dccd0878abb18e4bb61889a
Higor Pereira <madhigor@hotmail.com>
2020-12-25 16:33:24 -0300

    Using SSR to fetch data from GraphQL and passing it as props

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -1,29 +1,44 @@
-import React from 'react'
-import Link from 'next/link'
-import { NextRouter, useRouter } from 'next/router'
-import { GetServerSideProps } from 'next';
-import { AppProps } from 'next/app';
+import React from "react";
+import Link from "next/link";
+import { NextRouter, useRouter } from "next/router";
+import { GetServerSideProps } from "next";
+import { AppProps } from "next/app";
+import { Container, Main, Left, Right } from "../styles/UserStyles/styles";
+import Profile from '../components/Profile';
+import { USER_LIST } from './api/apolloClient/user/user';
+import { client } from "./api/apolloClient";
+import Header from "../components/Header";
 
 const User: React.FC<AppProps> = (props: AppProps) => {
+
   const router: NextRouter = useRouter();
   const { user } = router.query;
+
+
   return (
-    <div>
-      <Link href="/higor-dev">
-        <a>go to {user}</a>
-      </Link>
-    </div>
-  )
-}
+    <>
+      <Header/>
+      <Container>
+        <Main>
+          <Left>
+            <Profile data={props} />
+          </Left>
+          <Right></Right>
+        </Main>
+      </Container>
+    </>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async () => {
+
+  const { data } = await client.query({
+    query: USER_LIST
+  })
+
   return {
-    props: {
-      token: process.env.TOKEN
-    }
-  }
-}
+    props: data
+  };
+};
 
-export default User
-
-
+export default User;
