8a1ba30b28b409b57f5aa9ed685702fe45d3b2cf
Higor Pereira <madhigor@hotmail.com>
2020-12-25 16:26:13 -0300

    Adding Styled Components for Main page

---
+++ styles/UserStyles/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+
+export const Main = styled.div``;
+
+export const Left = styled.div``;
+
+export const Right = styled.div``;
