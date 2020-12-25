e7cd8542de53d62b38954158f5d330de52e68ec8
Higor Pereira <madhigor@hotmail.com>
2020-12-25 16:23:53 -0300

    Adjusting header styling

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -5,10 +5,37 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   background: var(--header);
-  padding: 11px 16px
+  padding: 14px 32px
 `;
 
-export const GithubLogo = styled(FaGithub)``;
+export const GithubLogo = styled(FaGithub)`
+
+fill: var(--logo);
+width: 33px;
+height: 33px;
+flex-shrink: 0;
 
 
-export const SearchForm = styled.form``;
+`;
+
+export const SearchForm = styled.form`
+padding-left: 16px;
+width: 100%;
+
+input {
+  background-color: var(--search);
+  min-height: 28px;
+  outline: none;
+  border-radius: 6px;
+  padding: 0 12px;
+  width: 150px;
+
+ &:focus {
+   width: 320px;
+ }
+
+ transition: width .2s ease-out, color .2s ease-out;
+
+}
+
+`;
