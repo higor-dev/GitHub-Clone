2da4745012d141d2522e11129c6d51d46ac843de
Higor Pereira <madhigor@hotmail.com>
2020-12-25 03:27:57 -0300

    Index page now redirects to user page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Router, { SingletonRouter } from 'next/router'
 
 export default function Home() {
+
+
+React.useEffect((): void => {
+   const {pathname}: SingletonRouter = Router
+   if(pathname == '/' ){
+       Router.push('/higor-dev')
+   }
+ });
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      
     </div>
   )
 }
