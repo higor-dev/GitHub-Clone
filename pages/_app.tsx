4398c4189cc1d852af2573bbf3f239d1ca24282d
Higor Pereira <madhigor@hotmail.com>
2020-12-25 16:31:52 -0300

    Wrapping the global component w/ Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app';
 import  GlobalStyle  from '../styles/globals'
 import Header from '../components/Header';
+import { ApolloProvider } from '@apollo/client';
+import {client } from './api/apolloClient/index'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <>
-    <Header />
+    <ApolloProvider client={client}>
     <Component {...pageProps} />
     <GlobalStyle />
+    </ApolloProvider>
   </>
 }
 
